bd67cdb93d410a9fc80a937230de7d4f2451d46e
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-10-17 15:55:08 +0300

    create general game cycle

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         System.out.println("Enter name for your hero:");
-        String hero_name  = buf.readLine();
+        String hero_name = buf.readLine();
 
         System.out.print("Available race:");
         System.out.println(Arrays.toString(race_list));
@@ -26,16 +26,24 @@ public class Main {
         System.out.println("Enter your profession:");
         String hero_prof = (buf.readLine()).toLowerCase();
 
-        Hero my_hero = new Hero(hero_name, hero_race,hero_prof);
+        Hero my_hero = new Hero(hero_name, hero_race, hero_prof);
         my_hero.create_hero();
-
-        int rnd = new Random().nextInt(enemy_list.length);
-        Enemy enemy = new Enemy(enemy_list[rnd]);
-        enemy.create_enemy();
-        fight_choice(my_hero,enemy);
+        while (true) {
+            Random rand = new Random();
+            int event = rand.nextInt(2);
+            if (event == 0) {
+                System.out.println("You meet nobody");
+            } else {
+                int rnd = new Random().nextInt(enemy_list.length);
+                Enemy enemy = new Enemy(enemy_list[rnd]);
+                enemy.create_enemy();System.out.printf("You meet %s. HP: %d. Damage: %d\n", enemy.name, enemy.hp, enemy.damage);
+                System.out.printf("Now, you have HP: %d and Damage:%d\n", my_hero.hp, my_hero.damage);
+                fight_choice(my_hero, enemy);
+            }
+    }
     }
     public static void fight_choice(Hero hero, Enemy enemy) throws IOException {
-            System.out.printf("Do you want to fight %s? (yes or no)", enemy.name);
+            System.out.printf("Do you want to fight %s? (yes or no)\n", enemy.name);
             String answer = buf.readLine();
             if (Objects.equals(answer.toLowerCase(), "yes")) {
                 boolean result = hero.hero_attack(enemy);
@@ -44,9 +52,9 @@ public class Main {
                     fight_choice(hero,enemy);
                 }
             } else if (Objects.equals(answer.toLowerCase(), "no")) {
-                System.out.printf("You run away from %s", enemy.name);
+                System.out.printf("You run away from %s\n", enemy.name);
             } else {
-                System.out.println("What?");
+                System.out.println("What?\n");
                 fight_choice(hero,enemy);
             }
     }
